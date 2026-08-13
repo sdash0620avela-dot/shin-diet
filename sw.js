422a4fdaba0cec0d5f99505308b58d307197c092
sdash0620avela-dot <sdash0620avela@icloud.com>
2026-08-13 10:35:20 +0900

    Refresh PWA cache for v14.1 multi-user release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shin-diet-v14-photo-api-fix-1';
+const CACHE='shin-diet-v14-1-multi-user-1';
 const APP_SHELL=['./index.html','./manifest.json','./icon.svg','./supabase-config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
