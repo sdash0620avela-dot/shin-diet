b286216a329f53da479213b97f6c9a1cfb0278a7
sdash0620avela-dot <sdash0620avela@icloud.com>
2026-08-13 22:03:08 +0900

    Refresh v15.3 app cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='shin-diet-v15-2-single-meal-confirm';
+const CACHE='shin-diet-v15-3-restore-protection';
 const APP_SHELL=['./index.html','./manifest.json','./icon.svg','./supabase-config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()).then(()=>self.clients.matchAll({type:'window'})).then(clients=>clients.forEach(client=>client.postMessage({type:'APP_UPDATED',version:'15.2'})))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()).then(()=>self.clients.matchAll({type:'window'})).then(clients=>clients.forEach(client=>client.postMessage({type:'APP_UPDATED',version:'15.3'})))));
 self.addEventListener('fetch',e=>{
   const url=new URL(e.request.url);
   if(e.request.method!=='GET'||url.origin!==self.location.origin)return;
